e057cbbb8ba595d7cbb9ca2330e207f8b7b82278
splashley <ashleyjlhynes@gmail.com>
2020-12-28 23:07:25 -0500

    css changes

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -1,22 +1,24 @@
 function addToList() {
   const task = document.getElementById("toDoList-Input").value;
   const myList = document.querySelector("#myList");
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.className = "removeItem";
-  span.onclick = function () {
+  const taskDiv = document.createElement("div");
+  taskDiv.classList.add("task");
+  const deleteDiv = document.createElement("deleteDiv");
+  deleteDiv.className = "removeItem";
+  deleteDiv.onclick = function () {
     removeFromList(this);
   };
-  span.appendChild(document.createTextNode("(x)"));
+  deleteDiv.appendChild(document.createTextNode("X"));
 
   // Throw back an error message when Add is clicked on but the input is empty
   if (task == "") {
     alert("You need to enter a task");
   } else {
     // Pressing the add button should take the value from the input and add it into the UL list as a li
-    li.appendChild(document.createTextNode(task));
-    li.appendChild(span);
-    myList.appendChild(li);
+    myList.appendChild(taskDiv);
+    taskDiv.appendChild(document.createTextNode(task));
+    taskDiv.appendChild(deleteDiv);
+    // document.getElementById("todoList-right").appendChild(span);
   }
 }
 
